7bd144187379b742d2b0f718c899dcf0c3290be9
Endrich-Machado <endrich@alunos.utfpr.edu.br>
2022-05-17 14:35:52 -0300

    Ajustes de pastas

---
+++ scripts/form.js
@@ -0,0 +1,27 @@
+/*const button = document.querySelector('#submit_button');
+
+button.addEventListener('click', () => {
+    const login = document.querySelector('#login').value;
+    const password = document.querySelector('#password').value;
+
+    if(login == "123" && password == "123"){
+        alert('Deu boa !!!');
+    }
+
+});*/
+
+
+//
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', () => {
+    const login = document.querySelector('#login').value;
+    const password = document.querySelector('#password').value;
+
+    if(login == "123" && password == "123"){
+        alert('Deu boa !!!');
+    }
+
+});
+
