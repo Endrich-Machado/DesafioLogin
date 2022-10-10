a89d369a498e8ae820fa3582297e5d5d4c6b4397
Endrich-Machado <endrich@alunos.utfpr.edu.br>
2022-10-10 14:49:08 -0300

    primeiro commit

--- scripts/form.js
+++ scripts/form.js
@@ -1,17 +1,3 @@
-/*const button = document.querySelector('#submit_button');
-
-button.addEventListener('click', () => {
-    const login = document.querySelector('#login').value;
-    const password = document.querySelector('#password').value;
-
-    if(login == "123" && password == "123"){
-        alert('Deu boa !!!');
-    }
-
-});*/
-
-
-//
 
 const form = document.querySelector('#form');
 
